4bed638cf593398caea0eaf025f262272facea83
Infratop <Infratop@infratopnoMacBook-Air.local>
2018-07-11 17:59:59 +0900

    finish 3-5

---
+++ sample_app/app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+ 
+    def new
+    	 @post = Post.new
+    end
+
+    def create
+              # ストロングパラメーターを使用
+    	post = Post.new(post_params)
+    	      # DBへ保存する
+    	post.save
+              # トップ画面へリダイレクト
+       redirect_to '/top'
+    end
+
+	private
+
+    def post_params
+    	params.require(:post).permit(:title, :body)
+
+    end
+end
+
+
+
---
+++ sample_app/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'posts/new'
+
+  get '/top' => 'root#top'
+
+  post '/posts' => 'posts#create'
+end
+
